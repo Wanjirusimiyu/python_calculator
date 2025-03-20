34c7540535f62c7ec2aba992d21e006ac962c25b
Wanjirusimiyu <wanjirusimiyu@gmail.com>
2025-03-20 15:47:00 +0300

    Comment the code

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
+# Create a calculator that asks a uset to input two numbers and an operation and perform an operation on the number
+# then print the result
+
 num_1 = int(input("Enter a number: "))
 num_2 = int(input("Enter another number: "))
 operation = input("Enter an operation (+, -, *, /): ")
 
+#if-elif statement to perform the operations based on user inputs.
 if operation == '+':
     result = num_1 + num_2
     print(result)
